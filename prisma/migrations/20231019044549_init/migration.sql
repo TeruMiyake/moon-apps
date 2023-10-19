e327adec9fb1d836e0bdb8d8f7acb4549265daa4
TeruMiyake <miyake@terum.jp>
2023-10-19 04:52:01 +0000

    feat: enable to connect to DB

---
+++ prisma/migrations/20231019044549_init/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE `ItemRank` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Item` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `rankId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Item` ADD CONSTRAINT `Item_rankId_fkey` FOREIGN KEY (`rankId`) REFERENCES `ItemRank`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;

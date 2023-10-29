0f7762fc5f7b6bcf0b23a7f2d4a4f8a1057c3d44
TeruMiyake <miyake@terum.jp>
2023-10-29 05:11:20 +0000

    feat: refine styles of RecentUpgrades

--- app/upgrade/RecentUpgrades.tsx
+++ app/upgrade/RecentUpgrades.tsx
@@ -15,6 +15,30 @@ export type IncludedUpgrade = Upgrade & {
   resultType: UpgradeResultType;
 };
 
+function toJTC(utc: Date): Date {
+  return new Date(utc.getTime() + 9 * 60 * 60 * 1000);
+}
+
+// 23.01.23 12:34 形式
+function toFormattedJTC_YYMMDDHHmm(utc: Date): string {
+  const jtc = toJTC(utc);
+  const year = jtc.getFullYear();
+  const month = jtc.getMonth() + 1;
+  const date = jtc.getDate();
+  const hours = jtc.getHours();
+  const minutes = jtc.getMinutes();
+  return `${year % 100}/${month.toString().padStart(2, "0")}/${date
+    .toString()
+    .padStart(2, "0")} ${hours.toString().padStart(2, "0")}:${minutes
+    .toString()
+    .padStart(2, "0")}`;
+}
+
+// 23.01.23 形式
+function toFormattedJTC_YYMMDD(utc: Date): string {
+  return toFormattedJTC_YYMMDDHHmm(utc).slice(0, 8);
+}
+
 export default async function RecentUpgrades() {
   const upgrades: IncludedUpgrade[] = await prisma.upgrade.findMany({
     take: 20,
@@ -27,16 +51,45 @@ export default async function RecentUpgrades() {
     },
   });
   return (
-    <div>
-      <h2 className="mb-2 text-2xl font-semibold">最近登録された合成結果</h2>
-      <ul className="list-inside list-disc">
-        {upgrades.map((upgrade) => (
-          <li key={upgrade.id} className="mb-1">
-            ランク {upgrade.rank.name}, +{upgrade.originalLevel} →, 結果:{" "}
-            {upgrade.resultType.name}
-          </li>
-        ))}
-      </ul>
+    <div className="max-w-2xl rounded bg-gray-100 p-4">
+      <h2 className="mb-4 text-2xl font-semibold text-gray-600">
+        最近登録された合成結果
+      </h2>
+      <table className="min-w-full bg-white text-center">
+        <thead>
+          <tr>
+            <th className="border-b px-4 py-2">登録日</th>
+            <th className="border-b px-4 py-2">合成日</th>
+            <th className="border-b px-4 py-2">ランク</th>
+            <th className="border-b px-4 py-2">元の強化値</th>
+            <th className="border-b px-4 py-2">結果</th>
+            <th className="border-b px-4 py-2">操作</th>
+          </tr>
+        </thead>
+        <tbody>
+          {upgrades.map((upgrade, index) => (
+            <tr
+              key={upgrade.id}
+              className={index % 2 === 0 ? "bg-gray-100" : ""}
+            >
+              <td className="border-b px-4 py-2">
+                {toFormattedJTC_YYMMDDHHmm(upgrade.registeredAt)}
+              </td>
+              <td className="border-b px-4 py-2">
+                {toFormattedJTC_YYMMDD(upgrade.triedAt)}
+              </td>
+              <td className="border-b px-4 py-2">{upgrade.rank.name}</td>
+              <td className="border-b px-4 py-2">{`+${upgrade.originalLevel}`}</td>
+              <td className="border-b px-4 py-2">{upgrade.resultType.name}</td>
+              <td className="border-b px-4 py-2">
+                <button className="rounded bg-red-500 px-4 py-2 text-white">
+                  削除
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
